2413c71069ce1c4c91eef28281b63c510aa3bc2d
XY56RE <xavier.mayeur@ing.com>
2022-02-14 17:33:59 +0100

    refactor functions on relationships

--- python/removeDefaultFolders.py
+++ python/removeDefaultFolders.py
@@ -6,18 +6,20 @@ if len(sys.argv) > 1:
     file = sys.argv[1]
 
     print('Removing default Archimate folders')
-    data = xmltodict.parse(open(file, 'r').read())
+    raw = open(file, 'r', encoding='utf8', errors="ignore").read()
+    data = xmltodict.parse(raw,  encoding='utf-8')
     items = data["model"]["organizations"]["item"]
     folders = []
     for i in items:
         if "label" in i:
-            label = i["label"]["#text"]
-            folders.append(i["item"])
+            if '#text' in i["label"]:
+                label = i["label"]["#text"]
+                folders.append(i["item"])
 
     data["model"]["organizations"]["item"] = folders
     result = xmltodict.unparse(data, pretty=True)
-    dir = os.path.dirname(file)
-    file = os.path.basename(file)
+    # dir = os.path.dirname(file)
+    # file = os.path.basename(file)
     # open(os.path.join(dir, file.split('.')[0]+'_conv.xml'), 'w').write(result)
-    open(file, 'w').write(result)
+    open(file, 'w', encoding='utf8').write(result)
 
