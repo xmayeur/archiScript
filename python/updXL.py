edf1a96e8a4c533de51f96bb74272b9ee84412b9
XY56RE <xavier.mayeur@ing.com>
2022-03-16 23:04:23 +0100

    Python script to support some CSV import function

--- python/updXL.py
+++ python/updXL.py
@@ -3,18 +3,21 @@ from os import path, listdir
 import openpyxl as xl
 from openpyxl.styles import Alignment
 from openpyxl.worksheet.datavalidation import DataValidation
-
+import sys
 
 csv_path = r'C:\Users\XY56RE\PycharmProjects\p13596-architecture-model\CSV'
 xl_path = r'C:\Users\XY56RE\ING\BE Architecture CoE Application Landscape - General\BE Mainframe Decommissioning'
 xl_file = r'BE Mainframe Application Functional Information.xlsx'
-out_path = r'C:\Users\XY56RE\ING\BE Architecture CoE Application Landscape - General\BE Mainframe Decommissioning\AF new format'
+out_path = r'C:\Users\XY56RE\ING\BE Architecture CoE Application Landscape - General\BE Mainframe Decommissioning\Application Functional Information'
 xl_out = r'text.xlsx'
 csv_file = 'AIS_functional_export.csv'
 
 suffix = r' - BE Mainframe Application Functional Information.xlsx'
 
-files = listdir(csv_path)
+if len(sys.argv) == 0:
+    files = listdir(csv_path)
+else:
+    files = sys.argv[1:]
 
 for f in files:
     xl_out = path.join(out_path, f[:3]+suffix)
@@ -35,6 +38,10 @@ for f in files:
                 col += 1
             row += 1
 
+    ws.sheet_view.selection = [ws.sheet_view.selection[0]]
+    ws.sheet_view.selection[0].pane = 'topLeft'
+    ws.freeze_panes = 'A2'
+
     if row > 2:
         # af_name= xl.worksheet.cell_range.CellRange("$F$3:$F$"+str(row), title=ws.title)
         af_range = xl.workbook.defined_name.DefinedName('AF', attr_text="'"+ws.title+"'!"+"$F$3:$F$"+str(row))
@@ -54,3 +61,4 @@ for f in files:
         bo_dv.add("B3:B459")
 
     wb.save(xl_out)
+
