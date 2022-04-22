2e8d312d0e97e4d44ce418ab20a863aa256d56ab
Xavier Mayeur <xavier.mayeur@ing.com>
2022-04-22 14:29:05 +0200

    Bug fixes

--- python/updXL.py
+++ python/updXL.py
@@ -1,29 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[189]:
+
+
+# ! pip install pandas openpyxl xlsxwriter
+
+
+# In[1]:
+
+
 import csv
 from os import path, listdir
+
 import openpyxl as xl
 from openpyxl.styles import Alignment
 from openpyxl.worksheet.datavalidation import DataValidation
-import sys
+
+# In[6]:
+
 
 csv_path = r'C:\Users\XY56RE\PycharmProjects\p13596-architecture-model\CSV'
 xl_path = r'C:\Users\XY56RE\ING\BE Architecture CoE Application Landscape - General\BE Mainframe Decommissioning'
 xl_file = r'BE Mainframe Application Functional Information.xlsx'
-out_path = r'C:\Users\XY56RE\ING\BE Architecture CoE Application Landscape - General\BE Mainframe Decommissioning\Application Functional Information'
+out_path = r'C:\Users\XY56RE\PycharmProjects\p13596-architecture-model\mfAppFiles\test'
 xl_out = r'text.xlsx'
 csv_file = 'AIS_functional_export.csv'
 
 suffix = r' - BE Mainframe Application Functional Information.xlsx'
 
-if len(sys.argv) == 0:
-    files = listdir(csv_path)
-else:
-    files = sys.argv[1:]
+# In[191]:
+
+
+# In[7]:
+
+
+files = listdir(csv_path)
 
 for f in files:
-    xl_out = path.join(out_path, f[:3]+suffix)
+    xl_out = path.join(out_path, f[:3] + suffix)
     print(xl_out)
 
-    wb = xl.load_workbook(path.join(xl_path, xl_file), read_only=False)
+    wb = xl.load_workbook(path.join(out_path, xl_file), read_only=False)
     wb.save(xl_out)
 
     ws = wb['Functional']
@@ -38,18 +56,14 @@ for f in files:
                 col += 1
             row += 1
 
-    ws.sheet_view.selection = [ws.sheet_view.selection[0]]
-    ws.sheet_view.selection[0].pane = 'topLeft'
-    ws.freeze_panes = 'A2'
-
     if row > 2:
-        # af_name= xl.worksheet.cell_range.CellRange("$F$3:$F$"+str(row), title=ws.title)
-        af_range = xl.workbook.defined_name.DefinedName('AF', attr_text="'"+ws.title+"'!"+"$F$3:$F$"+str(row))
+        af_name = xl.worksheet.cell_range.CellRange("F3:F" + str(row), title=ws.title)
+        af_range = xl.workbook.defined_name.DefinedName('AF', attr_text=af_name)
         wb.defined_names.append(af_range)
         af_dv = DataValidation(type="list", formula1='=AF', allow_blank=False)
 
-        # bo_name= xl.worksheet.cell_range.CellRange("$I$3:$I$"+str(row), title=ws.title)
-        bo_range = xl.workbook.defined_name.DefinedName('BO', attr_text="'"+ws.title+"'!"+"$I$3:$I$"+str(row))
+        bo_name = xl.worksheet.cell_range.CellRange("I3:I" + str(row), title=ws.title)
+        bo_range = xl.workbook.defined_name.DefinedName('BO', attr_text=bo_name)
         wb.defined_names.append(bo_range)
         bo_dv = DataValidation(type="list", formula1='=BO', allow_blank=False)
 
@@ -62,3 +76,4 @@ for f in files:
 
     wb.save(xl_out)
 
+# In[192]:
